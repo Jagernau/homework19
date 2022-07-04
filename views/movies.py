cabfff8a7f4533db88c8315c323897c425c4fc02
jagernau <stepanenkov25@gmail.com>
2022-07-04 23:19:44 +0300

    new5

--- views/movies.py
+++ views/movies.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_restx import Resource, Namespace
 
+from .helpers import auth_required, admin_required
+
 from dao.model.movie import MovieSchema
 from implemented import movie_service
 
@@ -9,6 +11,7 @@ movie_ns = Namespace('movies')
 
 @movie_ns.route('/')
 class MoviesView(Resource):
+    @auth_required
     def get(self):
         director = request.args.get("director_id")
         genre = request.args.get("genre_id")
@@ -22,6 +25,8 @@ class MoviesView(Resource):
         res = MovieSchema(many=True).dump(all_movies)
         return res, 200
 
+    @auth_required
+    @admin_required
     def post(self):
         req_json = request.json
         movie = movie_service.create(req_json)
@@ -30,18 +35,25 @@ class MoviesView(Resource):
 
 @movie_ns.route('/<int:bid>')
 class MovieView(Resource):
+
+    @auth_required
     def get(self, bid):
         b = movie_service.get_one(bid)
         sm_d = MovieSchema().dump(b)
         return sm_d, 200
-
+    
+    @auth_required
+    @admin_required
     def put(self, bid):
         req_json = request.json
         if "id" not in req_json:
             req_json["id"] = bid
         movie_service.update(req_json)
         return "", 204
+    
 
+    @auth_required
+    @admin_required
     def delete(self, bid):
         movie_service.delete(bid)
         return "", 204
